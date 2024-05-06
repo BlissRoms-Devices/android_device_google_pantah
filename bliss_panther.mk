d9a9243aff363036b8f5a72e69ef5e5e87c7703f
Aryan Arora <arora.aryan7@gmail.com>
2024-05-06 21:26:42 +0200

    Blissify
    Signed-off-by: Aryan Arora <arora.aryan7@gmail.com>

--- bliss_panther.mk
+++ bliss_panther.mk
@@ -4,20 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Bliss stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/gs201/bliss_common.mk)
 
-include device/google/pantah/panther/device-lineage.mk
+include device/google/pantah/panther/device-bliss.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := bliss_panther
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps
+TARGET_STOCK_GAPPS := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
