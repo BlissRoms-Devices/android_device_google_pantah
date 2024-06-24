c3321000348e5081cd10052a49f2b8bf91e86ea9
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 03:19:35 +0300

    pantah: Declare DM libs as symlinks during extraction
    Change-Id: Icb6c2c284037f67478c80725f63b4a7db7c552c0

--- Android.mk
+++ Android.mk
@@ -26,20 +26,13 @@ LOCAL_PATH := $(call my-dir)
 # their rules should be written here.
 
 ifneq (,$(filter $(TARGET_DEVICE),cloudripper ravenclaw cheetah panther))
-DM_LIBS := libdmengine.so libdmjavaplugin.so
-DM_32_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm/,$(notdir $(DM_LIBS)))
-$(DM_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "DMService 32 lib link: $@"
+HBM_LIBS := libhbmsvmanager_jni.so
+HBM_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT)/priv-app/HbmSVManager/lib/arm64/,$(notdir $(HBM_LIBS)))
+$(HBM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "HBM lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /product/lib/$(notdir $@) $@
+	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
 
-DM_64_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm64/,$(notdir $(DM_LIBS)))
-$(DM_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "DMService 64 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /product/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(DM_32_SYMLINKS) $(DM_64_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(HBM_SYMLINKS)
 endif
