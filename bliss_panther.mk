2b73cf21a19de7609d0721be5efa074df269ab9d
Jackeagle <jackeagle102@gmail.com>
2024-08-06 05:37:03 -0400

    pantah: Enable UDFPS Animation
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_panther.mk
+++ bliss_panther.mk
@@ -29,6 +29,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Gapps
 TARGET_STOCK_GAPPS := true
 
+# UDFPS
+EXTRA_UDFPS_ANIMATIONS := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="panther-user 15 AP3A.241105.007 12470370 release-keys" \
     BuildFingerprint=google/panther/panther:15/AP3A.241105.007/12470370:user/release-keys \
